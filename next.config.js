9890521890617fe3a887694f3ffbda983a3563be
seffu <seffukioi@gmail.com>
2022-04-06 10:03:05 +0300

    edits

--- next.config.js
+++ next.config.js
@@ -10,3 +10,11 @@ module.exports = {
     domains: ['res.cloudinary.com'],
   },
 }
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
