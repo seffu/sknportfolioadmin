427a4be8c8b741b6f27eaeee03eea487a2138fd8
seffu <seffukioi@gmail.com>
2022-04-06 05:59:47 +0300

    images

--- components/Header.js
+++ components/Header.js
@@ -1,46 +1,47 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
     return (
-        <nav class="layout-navbar navbar navbar-expand-lg align-items-lg-center bg-dark container-p-x" id="layout-navbar">
+        <nav className="layout-navbar navbar navbar-expand-lg align-items-lg-center bg-dark container-p-x" id="layout-navbar">
 
-        <a href="index.html" class="navbar-brand app-brand demo d-lg-none py-0 mr-4">
-            <span class="app-brand-logo demo">
-                <img src="assets/img/logo-dark.jpg" height="30" width="45" alt="Brand Logo" class="img-fluid"/>
+        <a href="index.html" className="navbar-brand app-brand demo d-lg-none py-0 mr-4">
+            <span className="app-brand-logo demo">
+                <Image src="/assets/img/logo-dark.jpg" height={30} width={45} alt="Brand Logo" className="img-fluid"/>
             </span>
         </a>
 
-        <div class="layout-sidenav-toggle navbar-nav d-lg-none align-items-lg-center mr-auto">
-            <a class="nav-item nav-link px-0 mr-lg-4" href="javascript:">
-                <i class="ion ion-md-menu text-large align-middle"></i>
+        <div className="layout-sidenav-toggle navbar-nav d-lg-none align-items-lg-center mr-auto">
+            <a className="nav-item nav-link px-0 mr-lg-4" href="javascript:">
+                <i className="ion ion-md-menu text-large align-middle"></i>
             </a>
         </div>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#layout-navbar-collapse">
-            <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#layout-navbar-collapse">
+            <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="navbar-collapse collapse" id="layout-navbar-collapse">
-            <hr class="d-lg-none w-100 my-2"/>
+        <div className="navbar-collapse collapse" id="layout-navbar-collapse">
+            <hr className="d-lg-none w-100 my-2"/>
 
-            <div class="navbar-nav align-items-lg-center ml-auto">
+            <div className="navbar-nav align-items-lg-center ml-auto">
 
-                <div class="nav-item d-none d-lg-block text-big font-weight-light line-height-1 opacity-25 mr-3 ml-1">|</div>
-                <div class="demo-navbar-user nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
-                        <span class="d-inline-flex flex-lg-row-reverse align-items-center align-middle">
-                            <img src="assets/img/avatars/1.png" alt class="d-block ui-w-30 rounded-circle"/>
-                            <span class="px-1 mr-lg-2 ml-2 ml-lg-0">Seffu Kioi</span>
+                <div className="nav-item d-none d-lg-block text-big font-weight-light line-height-1 opacity-25 mr-3 ml-1">|</div>
+                <div className="demo-navbar-user nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+                        <span className="d-inline-flex flex-lg-row-reverse align-items-center align-middle">
+                            <Image src="http://res.cloudinary.com/seffukioi/image/upload/v1649150513/portfolio/l8yx5aws3jmgvzmkpb11.jpg" layout='fill' alt className="d-block ui-w-30 rounded-circle"/>
+                            <span className="px-1 mr-lg-2 ml-2 ml-lg-0">Seffu Kioi</span>
                         </span>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-right">
+                    <div className="dropdown-menu dropdown-menu-right">
                         <Link href="profile">
-                        <a href="javascript:" class="dropdown-item"><i class="feather icon-user text-muted"></i> &nbsp; My profile</a>
+                            <a href="javascript:" className="dropdown-item"><i className="feather icon-user text-muted"></i> &nbsp; My profile</a>
                         </Link>
-                        <div class="dropdown-divider"></div>
+                            <div className="dropdown-divider"></div>
                         <Link href="signin">
-                        <a class="dropdown-item"> <i class="feather icon-power text-danger"></i> &nbsp; Log Out</a>
+                            <a className="dropdown-item"> <i className="feather icon-power text-danger"></i> &nbsp; Log Out</a>
                         </Link>
                     </div>
                 </div>
--- components/SideBar.js
+++ components/SideBar.js
@@ -4,86 +4,86 @@ import Link from 'next/link';
 const SideBar = () => {
     return (
         <>
-            <div id="layout-sidenav" class="layout-sidenav sidenav sidenav-vertical bg-dark logo-dark">
-                <div class="app-brand demo">
-                    <span class="app-brand-logo demo">
-                        <img src="assets/img/logo.jpg" height="30" width="45" alt="Brand Logo" class="img-fluid"/>
+            <div id="layout-sidenav" className="layout-sidenav sidenav sidenav-vertical bg-dark logo-dark">
+                <div className="app-brand demo">
+                    <span className="app-brand-logo demo">
+                        <img src="assets/img/logo.jpg" height="30" width="45" alt="Brand Logo" className="img-fluid"/>
                     </span>
-                    <a href="index.html" class="app-brand-text demo sidenav-text font-weight-normal ml-2">SKN portfolio</a>
-                    <a href="javascript:" class="layout-sidenav-toggle sidenav-link text-large ml-auto">
-                        <i class="ion ion-md-menu align-middle"></i>
+                    <a href="index.html" className="app-brand-text demo sidenav-text font-weight-normal ml-2">SKN portfolio</a>
+                    <a href="javascript:" className="layout-sidenav-toggle sidenav-link text-large ml-auto">
+                        <i className="ion ion-md-menu align-middle"></i>
                     </a>
                 </div>
-                <div class="sidenav-divider mt-0"></div>
+                <div className="sidenav-divider mt-0"></div>
 
-                <ul class="sidenav-inner py-1">
-                    <li class="sidenav-item open active">
+                <ul className="sidenav-inner py-1">
+                    <li className="sidenav-item open active">
                         <Link href='/'>
-                            <a class="sidenav-link">
-                                <i class="sidenav-icon feather icon-home"></i>
+                            <a className="sidenav-link">
+                                <i className="sidenav-icon feather icon-home"></i>
                                 <div>Dashboard</div>
                             </a>
                         </Link>
                     </li>
-                    <li class="sidenav-divider mb-1"></li>
-                    <li class="sidenav-item">
+                    <li className="sidenav-divider mb-1"></li>
+                    <li className="sidenav-item">
                         <Link href='/experience'>
-                            <a class="sidenav-link">
-                                <i class="sidenav-icon feather icon-octagon"></i>
+                            <a className="sidenav-link">
+                                <i className="sidenav-icon feather icon-octagon"></i>
                                 <div>Experience</div>
                             </a>
                         </Link>
                     </li>
-                    <li class="sidenav-divider mb-1"></li>
-                    <li class="sidenav-item">
+                    <li className="sidenav-divider mb-1"></li>
+                    <li className="sidenav-item">
                         <Link href='/education'>
-                            <a class="sidenav-link">
-                                <i class="sidenav-icon feather icon-award"></i>
+                            <a className="sidenav-link">
+                                <i className="sidenav-icon feather icon-award"></i>
                                 <div>Education</div>
                             </a>
                         </Link>
                     </li>
-                    <li class="sidenav-divider mb-1"></li>
-                    <li class="sidenav-item">
+                    <li className="sidenav-divider mb-1"></li>
+                    <li className="sidenav-item">
                         <Link href='/certifications'>
-                            <a class="sidenav-link">
-                                <i class="sidenav-icon feather icon-shield"></i>
+                            <a className="sidenav-link">
+                                <i className="sidenav-icon feather icon-shield"></i>
                                 <div>Certifications</div>
                             </a>
                         </Link>
                     </li>
-                    <li class="sidenav-divider mb-1"></li>
-                    <li class="sidenav-item">
+                    <li className="sidenav-divider mb-1"></li>
+                    <li className="sidenav-item">
                         <Link href='/technologies'>
-                            <a class="sidenav-link">
-                                <i class="sidenav-icon feather icon-activity"></i>
+                            <a className="sidenav-link">
+                                <i className="sidenav-icon feather icon-activity"></i>
                                 <div>Technologies</div>
                             </a>
                         </Link>
                     </li>
-                    <li class="sidenav-divider mb-1"></li>
-                    <li class="sidenav-item">
+                    <li className="sidenav-divider mb-1"></li>
+                    <li className="sidenav-item">
                         <Link href='/interests'>
-                            <a class="sidenav-link">
-                                <i class="sidenav-icon feather icon-thumbs-up"></i>
+                            <a className="sidenav-link">
+                                <i className="sidenav-icon feather icon-thumbs-up"></i>
                                 <div>Interests</div>
                             </a>
                         </Link>
                     </li>
-                    <li class="sidenav-divider mb-1"></li>
-                    <li class="sidenav-item">
+                    <li className="sidenav-divider mb-1"></li>
+                    <li className="sidenav-item">
                         <Link href='/projects'>
-                            <a class="sidenav-link">
-                                <i class="sidenav-icon feather icon-briefcase"></i>
+                            <a className="sidenav-link">
+                                <i className="sidenav-icon feather icon-briefcase"></i>
                                 <div>Projects</div>
                             </a>
                         </Link>
                     </li>
-                    <li class="sidenav-divider mb-1"></li>
-                    <li class="sidenav-item">
+                    <li className="sidenav-divider mb-1"></li>
+                    <li className="sidenav-item">
                         <Link href='/blogs'>
-                            <a class="sidenav-link">
-                                <i class="sidenav-icon feather icon-message-square"></i>
+                            <a className="sidenav-link">
+                                <i className="sidenav-icon feather icon-message-square"></i>
                                 <div>Blogs</div>
                             </a>
                         </Link>
